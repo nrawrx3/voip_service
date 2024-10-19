e93318a3169b9f9397d6e4924de0e994fcfd8a51
Soumik Rakshit <soumikrakshit@yandex.com>
2024-10-20 00:46:42 +0530

    Remove repl.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
-     tonic_build::configure()
+    tonic_build::configure()
         .out_dir("src/pb") // Output directory for the generated Rust files
         .compile(&["proto/voip_service_grpc.proto"], &["proto"]) // Proto file and its directory
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 
-    println!("cargo:rerun-if-changed=proto/voip_service.proto");
+    println!("cargo:rerun-if-changed=proto/voip_service_grpc.proto");
 }
